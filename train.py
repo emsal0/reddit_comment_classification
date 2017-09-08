6f24094c138a18742b3983b1d5f7a14ca3d0e41b
Em <emsal1863@gmail.com>
2017-09-08 11:41:46 -0700

    initial commit

---
+++ train.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
